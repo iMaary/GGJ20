7bbb576f28ecf360244b56e1e9d9d92311193913
gustavosalm <gustavosalmeida9821@gmail.com>
2020-02-01 14:40:15 -0300

    puzzle de setas pronto

--- GGJ20Unity/Assets/Scripts/RepairManBehaviour.cs
+++ GGJ20Unity/Assets/Scripts/RepairManBehaviour.cs
@@ -9,12 +9,14 @@ public class RepairManBehaviour : MonoBehaviour
     private GameObject[] instantArrows = new GameObject[4];
     private string seta;
     private GameObject oPuzzle;
+    private GameObject fixing;
 
     [SerializeField]
     private GameObject telaPuzzle;
     private bool onPuzzle = false;
     private int seq;
     public GameObject[] arrowsSr;
+    private int vida = 3;
 
     void Start()
     {
@@ -23,20 +25,20 @@ public class RepairManBehaviour : MonoBehaviour
 
     void Update()
     {
-        if (onPuzzle)
-            Puzzle();
+        if (onPuzzle) Puzzle();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "Corrompido")
         {
+            fixing = col.gameObject;
             oPuzzle = Instantiate(telaPuzzle, transform.position, Quaternion.identity);
             this.gameObject.GetComponent<PlayerMovement>().speed = 0;
             for(int i = 0; i < 4; i++)
             {
                 setasPuzzle[i] = Random.Range(0,4);
-                instantArrows[i] = Instantiate(arrowsSr[setasPuzzle[i]], transform.position - new Vector3(2 - i, 0), Quaternion.identity);
+                instantArrows[i] = Instantiate(arrowsSr[setasPuzzle[i]], transform.position - new Vector3(2 - (i * 1.3f), 0), Quaternion.identity);
                 print(setas[setasPuzzle[i]]);
             }
             onPuzzle = true;
@@ -46,24 +48,40 @@ public class RepairManBehaviour : MonoBehaviour
     {
         if (Input.GetKeyDown(setas[setasPuzzle[seq]]))
         {
-            //instantArrows[seq].GetComponent<Color>().a = 0.5f;
+            instantArrows[seq].GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 0.3f);
             seq++;
             if(seq == 4)
             {
-                Destroy(oPuzzle.gameObject);
-                this.gameObject.GetComponent<PlayerMovement>().speed = 7;
-                onPuzzle = false;
-                seq = 0;
-                for(int i = 0; i < 4; i++)
-                {
-                    Destroy(instantArrows[i].gameObject);
-                }
+                StartCoroutine("Repairing");
             }
         }
         else if (Input.anyKeyDown)
         {
-            print(1);
+            for (int i = 0; i <= seq; i++)
+            {
+                instantArrows[i].GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 1f);
+            }
             seq = 0;
         }
+    }
+
+    IEnumerator Repairing()
+    {
+        yield return new WaitForSeconds(0.4f);
+        Destroy(oPuzzle.gameObject);
+        this.gameObject.GetComponent<PlayerMovement>().speed = 7;
+        onPuzzle = false;
+        seq = 0;
+        //corsertar();
+        fixing.GetComponent<SpriteRenderer>().material.color = new Color(1f, 0f, 0f, 1f);
+        for (int i = 0; i < 4; i++)
+        {
+            Destroy(instantArrows[i].gameObject);
+        }
+    }
+
+    void TomarDano(int dmg)
+    {
+        vida = (vida - dmg < 0) ? 0 : vida - dmg;
     }
 }
