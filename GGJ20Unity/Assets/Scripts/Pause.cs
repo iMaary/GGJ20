38211a85672980035f8616e97a231d234cb49766
gustavosalm <gustavosalmeida9821@gmail.com>
2020-02-02 10:11:24 -0300

    menu e creditos movimentação dos virus c menos colisão

---
+++ GGJ20Unity/Assets/Scripts/Pause.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GetComponent<Animator>().SetBool("pausa", true);
+            Invoke("DesabilitaGamePlay", 1f);
+        }       
+    }
+    void DesabilitaGamePlay()
+    {
+        GameObject.FindGameObjectWithTag("DesabilitaGamePlay").gameObject.SetActive(false);
+    }
+}
